fb6426bdb2948e274315ac1b020d438a86178142
abel <cesarabel@deltacopiers.com>
2017-05-02 22:57:07 -0500

    estado levantamiento fisico

--- dtracking/models.py
+++ dtracking/models.py
@@ -478,7 +478,7 @@ class Log_Gestion(models.Model):
                                                                                  self.gestion.fecha_asignacion.hour,
                                                                                  self.gestion.fecha_asignacion.minute))
         if self.estado == ESTADOS_LOG_GESTION[2][0]:
-            txt = "Levantamiento físico realizado."
+            txt = "Levantamiento fisico realizado."
         if self.estado == ESTADOS_LOG_GESTION[3][0]:
             txt = "Inicio del Proceso."
         if self.estado == ESTADOS_LOG_GESTION[4][0]:
