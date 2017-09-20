07e2062d4f311326a887d479859d2c3d15cf4c99
xangcastle <ABcq12!@>
2017-09-19 22:56:23 -0500

    Proforma

--- valuarte/settings.py
+++ valuarte/settings.py
@@ -135,6 +135,8 @@ GEOPOSITION_GOOGLE_MAPS_API_KEY = 'YOUR_API_KEY'
 
 
 # Variables para el envio por gmail
+
+
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'xangcastle@gmail.com'
