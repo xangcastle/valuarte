3be001088a303a00be33a60a2cbf3e42355ea4af
sebastian.norena.marquez <sebastian.norena.marquez@gmail.com>
2018-04-16 12:49:00 -0500

    add:respaldo

---
+++ dtracking/management/commands/backup_database_ftp.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from django.core import management
+
+
+class Command(BaseCommand):
+    help = "Prueba"
+
+    def handle(self, *args, **options):
+        management.call_command('dbbackup')
--- valuarte/settings.py
+++ valuarte/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'dtracking',
     'background_task',
     'base',
+    'dbbackup',
     'geoposition',
     'grappelli_dynamic_navbar',
     'import_export',
@@ -185,3 +186,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 # reporte diario
 EMAILS_REPORTE_DIARIO ="sistema@valuarte.com.ni,mario.rojas@valuarte@valuarte.com.ni,gerardo.calderon@valuarte.com.ni,paola.ubeda@valuarte.com.ni,elvis.rivera@segdel.com,gerencia@segdel.com,mario.rojas@valuarte.com.ni,gerencia@valuarte.com.ni"
 EMAILS_FACTURACION = "mario.rojas@valuarte.com.ni,gerencia@valuarte.com.ni"
+DBBACKUP_STORAGE = 'dbbackup.storage.filesystem_storage'
+DBBACKUP_STORAGE_OPTIONS = {'location': '/var/backups/valuarte'}
