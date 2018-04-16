dcafc43f248b032fa3b641e42f5a3dd9e2700e3c
sebastian.norena.marquez <sebastian.norena.marquez@gmail.com>
2018-04-16 15:57:22 -0500

    test

--- valuarte/settings.py
+++ valuarte/settings.py
@@ -188,7 +188,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap3"
 # reporte diario
 EMAILS_REPORTE_DIARIO ="sistema@valuarte.com.ni,mario.rojas@valuarte@valuarte.com.ni,gerardo.calderon@valuarte.com.ni,paola.ubeda@valuarte.com.ni,elvis.rivera@segdel.com,gerencia@segdel.com,mario.rojas@valuarte.com.ni,gerencia@valuarte.com.ni"
 EMAILS_FACTURACION = "mario.rojas@valuarte.com.ni,gerencia@valuarte.com.ni"
-DBBACKUP_STORAGE = 'dbbackup.storage.filesystem_storage'
+DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': '/var/backups/valuarte'}
 DBBACKUP_CONNECTORS = {
 'default': {
